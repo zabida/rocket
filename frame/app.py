9897a067c5de8b3afc6366c4292bcdb64faa07b3
zabida <33972865+zabida@users.noreply.github.com>
2022-09-19 19:01:38 +0800

    init

---
+++ frame/app.py
@@ -0,0 +1,5 @@
+from typing import Dict
+
+
+def application(env: Dict, set_header):
+    pass
---
+++ server.py
@@ -0,0 +1,56 @@
+import socket
+import sys
+from threading import Thread
+from typing import List, Tuple
+
+from frame.app import application
+
+
+# "200 OK  [('Content-Type', 'text/html;')]"
+
+
+class Server(object):
+    def __init__(self, port=9090):
+        self.port = port
+
+    def start_response(self, code, headers: List[Tuple[str]]):
+        pass
+
+    def parse(self, req: str):
+        pass
+        return ""
+
+    def deal(self, sock, ip_addr):
+        temp_len = 4096
+        recv_content = ""
+        while True:
+            recv_data = sock.recv(temp_len)
+            print(recv_data.decode("utf-8"))
+            recv_content += recv_data.decode("utf-8")
+            if not recv_data:
+                break
+
+        env = dict()
+        method, url, header = self.parse(recv_content)
+        res = application(env, self.start_response)
+        sock.send(res.encode("utf-8"))
+        sock.close()
+
+    def run(self, port):
+        sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        sock.bind(("", port))
+        sock.listen(1024)
+        while True:
+            new_sock, ip_addr = sock.accept()
+            t = Thread(target=self.deal, args=(new_sock, ip_addr))
+            t.setDaemon(True)
+            t.start()
+        sock.close()
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    port = int(args[1])
+    server = Server(port=port)
+    server.run(port)
